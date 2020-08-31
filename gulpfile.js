fb2e6f18955c9df7a22ae08bef60f7b7e9e336cc
LJ322 <general00760@gmail.com>
2020-08-31 23:22:48 +0300

    Автоматизировал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -16,19 +22,91 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+        .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.mozjpeg({progressive: true}),
+          imagemin.svgo()
+        ]))
+}
+
+exports.images = images;
+
+// WebP
+
+const webP = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("source/img"))
+}
+
+exports.webp = webP;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+             .pipe(svgstore())
+             .pipe(rename("sprite.svg"))
+             .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"
+     })
+            .pipe(gulp.dest("build"))
+}
+
+exports.copy = copy;
+
+// Delete
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
+
+// Build
+
+const build = gulp.series(
+      clean,
+      copy,
+      styles,
+      sprite
+    )
+
+exports.build = build;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
---
+++ source/js/script.js
@@ -0,0 +1,5 @@
+'use strict'
+
+if (document.querySelector('navigation__list')) {
+
+}
